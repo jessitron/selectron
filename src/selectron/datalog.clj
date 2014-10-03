b89b294c016001e6cb232044bfa61a65aefd9189
Jessica Kerr <jessitron@gmail.com>
2014-10-02 22:00:18 -0500

    Comments for datalog example

--- src/selectron/datalog.clj
+++ src/selectron/datalog.clj
@@ -2,13 +2,20 @@
   (:use [datomic.api :only (db q) :as d])
   )
 
-(defn zip-with-index [s]
+(defn- zip-with-index [s]
   (map vector s (range)))
 
-(defn evify [maps]
+;; maps to EAV (entity, attibute, value) triples
+(defn- evify [maps]
   (into [] (for [[m i] (zip-with-index maps)
                  [k v] m]
-             [i k v])))
+             [i k v]))) ;; index becomes entity; key is attribute; value is value.
+;; (evify hard-coded-data) yields
+;; [[1 :number 4] // an entity "1" has number two
+;;  [1 :animal "Raccoon"] // an entity "1" has animal Raccoon
+;;  [2 :number 8] // an entity "2" has number eight
+;;  [2 :animal "Tarantula"] // an entity "2" has animal Tarantula
+;; ...]
 
 (defn datalog-select [data id]
  (first (first (q
@@ -16,4 +23,14 @@
      :where
      [?m :animal ?name]
      [?m :number ~id]]
-   ( evify  data)))))
+   (evify data)))))
+
+;; the query says:
+;; [:find ?name     ;; find every value for name
+;; :where           ;; such that there exist a set of facts satisfying
+;;   [?m :animal ?name]  ;; some entity has animal of this name
+;;   [?m :number ~id]    ;; and the same entity has number of this id.
+
+; what future changes would make this design shine?
+; - lots of other little tables to do the same way
+; - more complicated queries
