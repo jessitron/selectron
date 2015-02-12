bc7ba8e4f25dc68535119e1ec7eeba709beea80a
Jessica Kerr <jessitron@gmail.com>
2015-02-11 21:29:31 -0600

    Add Eric's suggestion of for-when

--- src/selectron/core.clj
+++ src/selectron/core.clj
@@ -1,10 +1,21 @@
 (ns selectron.core)
 
+;; simplest possible
 (defn simple-select [data id-val]
   (:animal (first (filter #(= id-val (:number %)) data))))
 
+;; generified to make it more explicit in the arguments
 (defn my-select [out-fn data in-fn & check]
   (first (map out-fn (filter (comp (apply partial check) in-fn) data))))
 
 ;; what sort of future makes this design shine?
 ;; Never looking at it again!
+
+;;
+;; Eric Normand suggests a for+when
+;; https://twitter.com/ericnormand/status/516812371764215808
+
+(defn eric-select [data id-val]
+  (first (for [{id :number value :animal} data
+               :when (= id id-val)]
+          value)))
--- test/selectron/core_test.clj
+++ test/selectron/core_test.clj
@@ -12,13 +12,18 @@
 (defspec first-instinct-neg 10
   (select-spec-negative simple-select))
 
-(def specific-my-select #(my-select :animal %1 :number = %2))
 ;; "I want the animal from DATA given that number = ID"
+(def specific-my-select #(my-select :animal %1 :number = %2))
 (defspec my-select-pos 10
   (select-spec-positive specific-my-select))
 (defspec my-select-neg 10
   (select-spec-negative specific-my-select))
 
+;; Eric Normand's suggestion
+(defspec eric-pos
+  (select-spec-positive eric-select))
+(defspec eric-neg
+  (select-spec-negative eric-select))
 
 (defspec datalog-one 10
   (select-spec-positive selectron.datalog/datalog-select))
